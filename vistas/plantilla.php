bf0d45c96d79f130ae27a852d62b1b704ce0e125
Karlitoxz <electronicaysistemas@gmail.com>
2020-11-08 17:13:15 -0500

    Variables POST

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -31,18 +31,66 @@
 	<div class="container-fluid bg-light">
 		<div class="container">
 			<ul class="nav nav-justified py-2 nav-pills">
-				<li class="nav-item">
-					<a href="index.php?pagina=registro" class="nav-link ">Registro</a>
-				</li>
-				<li class="nav-item">
-					<a href="index.php?pagina=ingreso" class="nav-link">Ingreso</a>
-				</li>
-				<li class="nav-item">
-					<a href="index.php?pagina=inicio" class="nav-link active">Inicio</a>
-				</li>
-				<li class="nav-item">
-					<a href="index.php?pagina=salir" class="nav-link">Salir</a>
-				</li>
+
+				<?php if (isset($_GET['pagina']) && $_GET['pagina'] != ""): ?>
+
+					<?php if ($_GET['pagina'] == "registro"): ?>
+						<li class="nav-item">
+							<a class="nav-link active" href="index.php?pagina=registro">Registro</a>
+						</li>
+						<?php else: ?>
+							<li class="nav-item">
+								<a class="nav-link" href="index.php?pagina=registro">Registro</a>
+							</li>
+						<?php endif ?>
+
+						<?php if ($_GET['pagina'] == "ingreso"): ?>
+							<li class="nav-item">
+								<a class="nav-link active" href="index.php?pagina=ingreso">Ingreso</a>
+							</li>
+							<?php else: ?>
+								<li class="nav-item">
+									<a class="nav-link" href="index.php?pagina=ingreso">Ingreso</a>
+								</li>
+							<?php endif ?>
+
+							<?php if ($_GET['pagina'] == "inicio"): ?>
+								<li class="nav-item">
+									<a class="nav-link active" href="index.php?pagina=inicio">Inicio</a>
+								</li>
+								<?php else: ?>
+									<li class="nav-item">
+										<a class="nav-link" href="index.php?pagina=inicio">Inicio</a>
+									</li>
+								<?php endif ?>
+
+								<?php if ($_GET['pagina'] == "salir"): ?>
+									<li class="nav-item">
+										<a class="nav-link active" href="index.php?pagina=salir">Salir</a>
+									</li>
+									<?php else: ?>
+										<li class="nav-item">
+											<a class="nav-link" href="index.php?pagina=salir">Salir</a>
+										</li>
+									<?php endif ?>
+
+						<?php else: ?>
+							<li class="nav-item">
+								<a class="nav-link active" href="index.php?pagina=registro">Registro</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="index.php?pagina=ingreso">Ingreso</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="index.php?pagina=inicio">Inicio</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="index.php?pagina=salir">Salir</a>
+							</li>
+
+
+				<?php endif ?>
+
 			</ul>
 		</div>
 	</div>
@@ -62,7 +110,7 @@
 					 {
 						include "paginas/".$_GET['pagina'].".php";
 					} else {
-						include "paginas/registro.php";
+						include "paginas/error404.php";
 					}
 				
 			} else {
