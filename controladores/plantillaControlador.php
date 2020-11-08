e07ac565a0a0e5ed851b2e63eae7e5a9b7303a24
Karlitoxz <electronicaysistemas@gmail.com>
2020-11-08 16:17:40 -0500

    Navegar con variables GET

---
+++ controladores/plantillaControlador.php
@@ -0,0 +1,14 @@
+<?php 
+
+Class controladorPlantilla{
+
+	//llamar a la plantilla 
+	public function ctrTraerPlantilla(){
+		include "vistas/plantilla.php";
+	}
+
+
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	require_once "controladores/plantillaControlador.php";
+	
+	//instanciar objeto
+		$plantilla = new controladorPlantilla();
+		$plantilla -> ctrTraerPlantilla();
+
+?>
