ba23299d1ee9ae9de72de7f5f26aa7cde1018558
Karlitoxz <electronicaysistemas@gmail.com>
2021-04-08 14:25:26 -0500

    Subir archivos y parametros con cURL-PHP a un REST API : livehelper

--- cURL.php
+++ cURL.php
@@ -28,37 +28,60 @@
 /*
 
 Petición POST
-Enviamos datos a una URL destino.
-
-<?php
-    $fields = array('field1' => 'valor1', 'field2' => urlencode('valor 2'));
-    $fields_string = http_build_query($fields);
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://url-de-la-web/test");
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string );
-    $data = curl_exec($ch);
-    curl_close($ch);
-?>
-Explicación línea por línea:
-//Inicializamos nuestro arreglo de valores
-//Si nuestros valores contienen espacios o caracteres especiales
-//podemos convertirlos mediante urlencode("valor");
-$fields = array('field1' => 'valor1', 'field2' => urlencode('valor 2'));
-//convertimos el arreglo en formato URL
-$fields_string = http_build_query($fields);
-//Abrimos la conexión cURL
-$ch = curl_init();
-//Configuramos mediante CURLOPT_URL la URL destino
-curl_setopt($ch, CURLOPT_URL, "https://url-de-la-web/test");
-//Indicamos que se trata de una petición POST con valor "1" o "true"
-curl_setopt($ch, CURLOPT_POST, 1);
-//Asignamos los campos a enviar en el POST
-curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string );
-//Ejecuta la petición HTTP y almacena la respuesta en la variable $data.
-$data = curl_exec($ch);
-//Cierra la conexión cURL
-curl_close($ch);
 
 */
 ?>
+
+<?php 
+class SubirFilesLHCFiles
+{  
+
+
+
+   static public function subirArchivosLHCAPI($archivos, $num_id){
+
+   $archivos = $_FILES["files"];
+   $num_id = filter_input(INPUT_POST, 'num_id', FILTER_SANITIZE_NUMBER_INT);
+
+   $rutaAPI_LCH = 'https://'.$_SERVER["SERVER_NAME"].'/livehelper/index.php/restapi/file';
+
+   $username = 'user';
+   $password = 'pass';
+
+      $ch = curl_init();
+      
+      curl_setopt($ch, CURLOPT_URL, $rutaAPI_LCH);
+         //autenticacion:
+         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data'));
+         curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
+         //SSL
+         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+         //options
+         curl_setopt($ch, CURLOPT_MAXREDIRS, 10); 
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+
+       $cfile = new CURLFile($archivos['tmp_name'], $archivos['type'], $archivos['name']);
+
+      $data = array('files' => $cfile, 'num_id'=> $num_id);
+      echo '<pre>'; print_r($data); echo '</pre>';
+      
+      curl_setopt($ch, CURLOPT_POST,1);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+      // Execute the handle
+  $respuesta = curl_exec($ch);
+  echo '<pre>'; print_r($respuesta); echo '</pre>';
+
+     if ($respuesta == true) {
+         echo "Success";
+     }else{
+         echo "Error". curl_error($ch);
+     }
+   }
+}
+
+ ?>
