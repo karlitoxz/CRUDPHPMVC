3bb0a94f10eb2e693589f1c5320e1e2221008473
Juan Romero (SeguriHack) <electronicaysistemas@gmail.com>
2021-06-30 11:29:39 -0500

    Update response other example-laravel

--- HowresponsePHPAPI.php
+++ HowresponsePHPAPI.php
@@ -7,3 +7,16 @@
 		http_response_code(200);
 	}
 ?>
+
+<?php
+	
+	    public function dataTable(Request $fideubf){
+        if ($fideubf->ajax()) {
+            $observaciones = FISECOBSBF::select('idecli','valorpri','codefe','observ','usuari','fecha','hora')
+                            ->where('nitdeu', '=', $fideubf->fideubf)
+                            ->with('codefeDesc')
+                            ->get();
+            return response(json_encode($observaciones, JSON_INVALID_UTF8_IGNORE),200)->header('Content-type', 'application/json');
+        }    
+    }
+?>
